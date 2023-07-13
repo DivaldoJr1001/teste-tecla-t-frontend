07679133b1b94e31b25d8dd16420ae38d5a7980b
Divaldo Aderaldo de Oliveira Júnior <divaldojr9@gmail.com>
2023-07-13 16:48:11 -0300

    Implementação de arquitetura básica

---
+++ src/app/shared/material/visual.module.ts
@@ -0,0 +1,121 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {BidiModule} from '@angular/cdk/bidi';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatTreeModule,
+    MatTooltipModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSliderModule,
+    BidiModule,
+    DragDropModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatTreeModule,
+    MatTooltipModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSliderModule,
+    BidiModule,
+    DragDropModule,
+    MatRippleModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatSlideToggleModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule
+  ]
+})
+export class VisualModule {
+}
---
+++ src/app/tabs/liked-movies/liked-movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liked-movies',
+  templateUrl: './liked-movies.component.html',
+  styleUrls: ['./liked-movies.component.scss']
+})
+export class LikedMoviesComponent {
+
+}
